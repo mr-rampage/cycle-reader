1e5cfc9dc6a6f74d612580bbae9e14d456d460e2
mr-rampage <readonly@che.wbac.ca>
2017-07-21 21:27:51 -0400

    using input streams for AppendableList

--- src/components/item-list.js
+++ src/components/item-list.js
@@ -2,27 +2,23 @@ import { DomNode } from './dom-node';
 import Rx from 'rxjs/Rx';
 import { ComponentStream } from './component-stream';
 
-const ADD_ITEM_TO_LIST_EVENT = 'ADD_ITEM';
+export function AppendableItemList(addItem$) {
+  const list = DomNode('<ul></ul>');
 
-export function ItemList() {
-  const list = AppendableList('<ul></ul>', ADD_ITEM_TO_LIST_EVENT);
+  addItem$.subscribe(item => list.appendChild(Item(item)));
 
-  const stream = Rx.Observable.fromEvent(list, 'click')
+  const removeItem$ = Rx.Observable.fromEvent(list, 'click')
     .filter(event => /li/i.test(event.srcElement.tagName))
     .pluck('srcElement');
-  stream.subscribe(item => list.removeChild(item));
 
-  return ComponentStream(list, stream.map(item => item.textContent));
-}
+  removeItem$.subscribe(item => list.removeChild(item));
 
-export function AddToItemListEvent(description) {
-  return new CustomEvent(ADD_ITEM_TO_LIST_EVENT, {detail: description});
-}
+  const stream = Rx.Observable.merge(
+    removeItem$.map(item => ({'removed': item.textContent})),
+    addItem$.map(item => ({'added': item}))
+  ).share();
 
-function AppendableList(template, appendEventName) {
-  const list = DomNode(template);
-  list.addEventListener(appendEventName, event => list.appendChild(Item(event.detail)));
-  return list;
+  return ComponentStream(list, stream);
 }
 
 function Item(description) {
--- src/components/item-list.spec.js
+++ src/components/item-list.spec.js
@@ -1,3 +1,30 @@
-describe('ItemList', () => {
+import { AppendableItemList } from './item-list';
+import Rx from 'rxjs';
+
+describe('AppendableItemList', () => {
+
+  let fixture;
+  let input$;
+
+  beforeEach(() => {
+    input$ = new Rx.Subject();
+    fixture = AppendableItemList(input$)
+  });
+
+  it('should start with an empty list', () => {
+    expect(fixture.component.childNodes.length).toBe(0);
+  });
+
+  it('should add an item from the input stream', () => {
+    fixture.stream.subscribe(added => expect(added).toEqual({'added': 'foo'}));
+    input$.next('foo');
+    expect(fixture.component.childNodes.length).toBe(1);
+  });
+
+  it('should remove an item from the list on click', () => {
+    input$.next('foo');
+    fixture.stream.subscribe(removed => expect(removed).toEqual({'removed': 'foo'}));
+    fixture.component.firstChild.click();
+  });
 
 });
