b08212cc5fbcc38ae27f9ac0c45ed728dd789187
mr-rampage <readonly@che.wbac.ca>
2017-07-26 23:34:21 -0400

    removed list's dependency on feeds

--- src/components/rss-list.js
+++ src/components/rss-list.js
@@ -2,21 +2,14 @@ import { AppendableItemList } from './item-list';
 import { RssUri } from './rss-uri';
 import { DomNode } from './dom-node';
 import { ComponentStream } from './component-stream';
-import { RssFeed } from './rss-feed';
 
 export function RssList() {
   const rssUri = RssUri();
   const rssList = AppendableItemList(rssUri.stream);
-  const rssFeed = RssFeed(createAddedFeed$(rssList.stream));
 
   const rssListContainer = DomNode('<div></div>');
   rssListContainer.appendChild(rssList.component);
   rssListContainer.appendChild(rssUri.component);
-  rssListContainer.appendChild(rssFeed.component);
 
   return ComponentStream(rssListContainer, rssList.stream);
-}
-
-function createAddedFeed$(rssList$) {
-  return rssList$.filter(event => event.added).pluck('added');
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import './service-workers/cache.service-worker';
 import './assets/styles/styles.scss';
 import { RssList } from './components/rss-list';
+import { RssFeed } from './components/rss-feed';
 
 require('html-loader!./templates/index.html');
 
 const list = RssList();
+const feed = RssFeed(list.stream.filter(event => event.added).pluck('added'));
 document.body.appendChild(list.component);
+document.body.appendChild(feed.component);
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/cache.service-worker.js');
 }
+
