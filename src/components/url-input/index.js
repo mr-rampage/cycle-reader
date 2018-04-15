15a66acd6a0066efde5d42731ac4a600bbbe8cae
mr-rampage <readonly@che.wbac.ca>
2018-04-15 15:30:16 -0400

    feat(search): adding search

--- src/components/url-input/index.js
+++ src/components/url-input/index.js
@@ -1,14 +1,21 @@
 import { Search } from './search'
+import xs from 'xstream'
 
 const isUrl = new RegExp('^(https?|ftp):\\/\\/[^\\s/$.?#].[^\\s]*$', 'i')
 
 export function UrlInput (sources) {
-  const intent = sources => sources.DOM
-    .select('.uk-search-input')
-    .events('input')
-    .map(event => event.target.value)
+
+  const onSubmit = sources => sources.DOM.select('.uk-search').events('submit')
+  const onInput = sources => sources.DOM.select('.uk-search-input').events('input')
+
+  const intent = sources => xs.combine(onSubmit(sources), onInput(sources))
+    .map(([submitEvent, inputEvent]) => {
+      submitEvent.preventDefault()
+      return inputEvent.target.value
+    })
 
   const model = intent$ => intent$
+    .debug()
     .filter(input => isUrl.test(input))
     .startWith('')
 
--- src/components/url-input/search.jsx
+++ src/components/url-input/search.jsx
@@ -1,7 +1,7 @@
 export function Search () {
   return (
     <form className="uk-search uk-search-large">
-      <span uk-search-icon></span>
+      <span attrs={{'uk-search-icon': true}}></span>
       <input className="uk-search-input" type="search" placeholder="Search"/>
     </form>
   )
