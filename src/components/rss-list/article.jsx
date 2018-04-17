32724332a6b79163fc1ce57e13535d743c2bab67
mr-rampage <readonly@che.wbac.ca>
2018-04-16 22:40:22 -0400

    refactor(Article): cleaning up interface

--- src/components/rss-list/article.jsx
+++ src/components/rss-list/article.jsx
@@ -1,13 +1,13 @@
-export function Article (feedItem) {
+export function Article ({title, date, description, thumbnail}) {
   return (
     <div className="uk-card uk-card-default uk-grid-collapse uk-margin-large uk-padding-small" attrs={{'uk-grid': true}}>
       <div className="uk-card-media-left uk-cover-container uk-width-1-3@s">
-        <span innerHTML={feedItem.thumbnail} attrs={{'uk-cover': true}}></span>
+        <span innerHTML={thumbnail} attrs={{'uk-cover': true}}></span>
       </div>
       <div className="uk-card-body uk-width-expand@m">
-        <h3 className="uk-card-title">{feedItem.title}</h3>
-        <p className="uk-text-meta">{feedItem.date}</p>
-        <p innerHTML={feedItem.description}></p>
+        <h3 className="uk-card-title">{title}</h3>
+        <p className="uk-text-meta">{date}</p>
+        <p innerHTML={description}></p>
       </div>
     </div>
   )
