7ae75047ee28c18efc93101ecc6f3d8d5819e4f8
mr-rampage <readonly@che.wbac.ca>
2018-04-25 00:14:30 -0400

    feat(fetch): dropping multiple requests

--- src/drivers/cycle-fetch-driver.js
+++ src/drivers/cycle-fetch-driver.js
@@ -1,18 +1,23 @@
 import xs from 'xstream'
 import 'whatwg-fetch'
-import {adapt} from '@cycle/run/lib/adapt'
+import { adapt } from '@cycle/run/lib/adapt'
+import dropRepeats from 'xstream/extra/dropRepeats'
 
 export function makeFetchDriver () {
   return function fetchDriver (request$) {
     const response$$ = request$
+      .debug()
+      .compose(dropRepeats(sameRequest))
       .map(requestsInputToResponse$)
+
     response$$.addListener({
       next: () => {},
       error: () => {},
       complete: () => {}
     })
+
     return {
-      select: byCategory.bind(null, response$$)
+      select: select.bind(null, response$$)
     }
   }
 }
@@ -31,10 +36,9 @@ function requestsInputToResponse$ (request) {
   return response$
 }
 
-function createResponse$ (request) {
+function createResponse$ ({url, options}) {
   return xs.create({
     start: listener => {
-      const {url, options} = request
       fetch(url, options)
         .then(response => response.json())
         .then(response => listener.next(response))
@@ -44,7 +48,11 @@ function createResponse$ (request) {
   })
 }
 
-function byCategory (response$$, category) {
+function select (response$$, category) {
   return adapt(response$$
     .filter(response$ => response$.request && response$.request.category === category))
+}
+
+function sameRequest (previous, current) {
+  return previous.url === current.url
 }
