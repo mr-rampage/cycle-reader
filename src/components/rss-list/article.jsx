79cb22f4647759c9b43a298ed683014fe3ef13e6
mr-rampage <readonly@che.wbac.ca>
2018-04-27 00:14:18 -0400

    fix(article): margins

--- src/components/rss-list/article.jsx
+++ src/components/rss-list/article.jsx
@@ -1,7 +1,7 @@
 export function Article ({source, title, created, description, thumbnail, link}) {
   return (
-    <div className="uk-card uk-card-default uk-grid-collapse uk-margin-large uk-padding-small" attrs={{'uk-grid': true, 'href': link}}>
-      <div className="uk-card-media-left uk-cover-container uk-width-1-3@s">
+    <div className="uk-card uk-card-default uk-margin-medium uk-padding-small" attrs={{'uk-grid': true, 'href': link}}>
+      <div className="uk-card-media-left uk-cover-container uk-width-1-3@m">
         <span innerHTML={thumbnail} attrs={{'uk-cover': true}}></span>
       </div>
       <div className="uk-card-body uk-width-expand@m">
