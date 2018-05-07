bb90b1d66f953cc4f600088cefb5b741da15469b
mr-rampage <readonly@che.wbac.ca>
2018-05-06 23:49:12 -0400

    fix(cards): fixing padding issue

--- src/components/article-list/article.jsx
+++ src/components/article-list/article.jsx
@@ -1,6 +1,6 @@
 export function Article ({source, title, isoDate, description, thumbnail, link}) {
   return (
-    <div className="uk-card uk-card-default uk-margin-medium uk-padding-small" attrs={{'uk-grid': true, 'href': link}}>
+    <div className="uk-card uk-card-default uk-grid-collapse uk-margin-medium uk-padding-small" attrs={{'uk-grid': true, 'href': link}}>
       <div className="uk-card-media-left uk-cover-container uk-width-1-3@m">
         <span attrs={{'uk-cover': true}}>
           <img src={thumbnail} />
