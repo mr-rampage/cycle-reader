37e7e40a8c1201e6558e9082d03d5ba42181edde
mr-rampage <readonly@che.wbac.ca>
2018-05-11 23:29:27 -0400

    feat(fetch): added support for json message type

--- src/workers/article.worker.js
+++ src/workers/article.worker.js
@@ -2,6 +2,22 @@ import 'whatwg-fetch'
 
 onmessage = e =>
   fetch(e.data.url, e.data.options)
-    .then(response => response.text())
-    .then(response => ({body: response, request: e.data}))
+    .then(deserializeResponse)
+    .then(adaptResponseBody.bind(null, e.data))
     .then(postMessage)
+
+function deserializeResponse (response) {
+  let contentType = response.headers.get('content-type')
+  if (/application\/json/.test(contentType)) {
+    return response.json()
+  } else {
+    return response.text()
+  }
+}
+
+function adaptResponseBody (request, response) {
+  return {
+    body: response.body || response,
+    request
+  }
+}
