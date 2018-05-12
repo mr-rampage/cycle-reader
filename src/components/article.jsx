fc85018068b1cd8ed206b6a1801d41843b559231
mr-rampage <readonly@che.wbac.ca>
2018-05-12 00:21:02 -0400

    fix(cards): images disappearing on small screens

--- src/components/article.jsx
+++ src/components/article.jsx
@@ -1,12 +1,11 @@
 export function Article ({source, title, isoDate, description, thumbnail, link}) {
   return (
     <div className="uk-card uk-card-default uk-grid-collapse uk-margin-medium uk-padding-small" attrs={{'uk-grid': true, 'href': link}}>
-      <div className="uk-card-media-left uk-cover-container uk-width-1-3@m">
-        <span attrs={{'uk-cover': true}}>
-          <img src={thumbnail} />
-        </span>
+      <div className="uk-card-media-left uk-cover-container uk-width-1-3@m" attrs={{'hidden': !thumbnail}}>
+        <img src={thumbnail} attrs={{'uk-cover': true}} />
+        <canvas width="200" height="300"></canvas>
       </div>
-      <div className="uk-card-body uk-width-expand@m">
+      <div className="uk-card-body uk-width-expand@s">
         <div className="uk-card-badge uk-label">{source}</div>
         <h3 className="uk-card-title">{title}</h3>
         <p className="uk-text-meta">{isoDate}</p>
