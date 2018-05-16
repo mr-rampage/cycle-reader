db852b91f488d9cb7048b5cb986dff44d6d4420e
mr-rampage <readonly@che.wbac.ca>
2018-05-15 21:48:48 -0400

    refactor(ui): shrink search bar

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,6 +1,6 @@
 export function Search () {
   return (
-    <form className="uk-search uk-search-large uk-width-1-1">
+    <form className="uk-search uk-search-default uk-width-1-1">
       <span attrs={{'uk-search-icon': true}}></span>
       <input className="uk-search-input" type="search" placeholder="Search" value="http://"/>
     </form>
