4f7f94ea567fedd0225cb006b184c756a5d0b84d
mr-rampage <readonly@che.wbac.ca>
2018-05-19 21:46:28 -0400

    refactor(test): replacing tests using mocha

--- scripts/test.js
+++ scripts/test.js
@@ -8,6 +8,8 @@ const mocha = path.resolve(process.cwd(), 'node_modules', '.bin', 'mocha')
 
 const args = [
   '--colors',
+  '--require',
+  'babel-register',
   !process.env.CI && (console.log(chalk.green.bold('Enabling watch mode')) || '--watch'),
   'src/**/*.spec.js'
 ].filter(Boolean)
--- src/drivers/cycle-fetch-driver.spec.js
+++ src/drivers/cycle-fetch-driver.spec.js
@@ -1,49 +1,49 @@
-import test from 'ava'
 import { makeFetchDriver } from './cycle-fetch-driver'
 import { mockTimeSource } from '@cycle/time'
 
-test.beforeEach(t => {
+describe('Cycle Fetch Driver', () => {
   const fakeResponse = {}
+  let fixture
 
-  t.context.Time = mockTimeSource()
-  t.context.driver = makeFetchDriver()
-  t.context.fakeResponse = fakeResponse
+  beforeEach(() => {
+    fixture = makeFetchDriver()
 
-  global.fetch = function mockFetch (url) {
-    return Promise.resolve({
-      url,
-      status: 200,
-      statusText: 'OK',
-      ok: true,
-      json: () => fakeResponse
+    global.fetch = function mockFetch (url) {
+      return Promise.resolve({
+        url,
+        status: 200,
+        statusText: 'OK',
+        ok: true,
+        json: () => fakeResponse
+      })
+    }
+  })
+
+  it('should drop requests from the same url', done => {
+    const Time = mockTimeSource()
+
+    const request$ = Time.diagram('a--b---c|', {
+      a: {url: 'same', category: 'bar'},
+      b: {url: 'same', category: 'dog'},
+      c: {url: 'same', category: 'bar'}
     })
-  }
-})
+    const expected$ = Time.diagram('a', {a: fakeResponse})
 
-test.cb('should drop requests for the same url', t => {
-  const Time = t.context.Time
-
-  const request$ = Time.diagram('a--b---c|', {
-    a: {url: 'same', category: 'bar'},
-    b: {url: 'same', category: 'dog'},
-    c: {url: 'same', category: 'bar'}
+    Time.assertEqual(fixture(request$).select('bar').flatten(), expected$)
+    Time.run(done)
   })
-  const expected$ = Time.diagram('a', {a: t.context.fakeResponse})
 
-  Time.assertEqual(t.context.driver(request$).select('bar').flatten(), expected$)
-  Time.run(t.end)
-})
+  it('should select responses by category', done => {
+    const Time = mockTimeSource()
 
-test.cb('should select responses by category', t => {
-  const Time = t.context.Time
+    const request$ = Time.diagram('a--b---c|', {
+      a: {url: 'same', category: 'bar'},
+      b: {url: 'different', category: 'dog'},
+      c: {url: 'similar', category: 'bar'}
+    })
+    const expected$ = Time.diagram('a------c', {a: fakeResponse, c: fakeResponse})
 
-  const request$ = Time.diagram('a--b---c|', {
-    a: {url: 'same', category: 'bar'},
-    b: {url: 'different', category: 'dog'},
-    c: {url: 'similar', category: 'bar'}
+    Time.assertEqual(fixture(request$).select('bar').flatten(), expected$)
+    Time.run(done)
   })
-  const expected$ = Time.diagram('a------c', {a: t.context.fakeResponse, c: t.context.fakeResponse})
-
-  Time.assertEqual(t.context.driver(request$).select('bar').flatten(), expected$)
-  Time.run(t.end)
 })
